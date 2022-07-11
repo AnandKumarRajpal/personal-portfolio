2f81d2dd3808d03c3f6e0438fcf3d473fcf20e4d
Anand Kumar <anand.kumar@securiti.ai>
2022-07-11 23:25:39 +0500

    Increased vertical width

--- src/components/ProgrammingLanguages.vue
+++ src/components/ProgrammingLanguages.vue
@@ -116,7 +116,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .pro-lang {
   filter: grayscale(100%);
   -webkit-filter: grayscale(100%);
@@ -125,5 +125,8 @@ export default {
 .pro-lang:hover {
   filter: none;
   -webkit-filter: grayscale(0);
+}
+.v-divider--vertical {
+  border-width: 0 5px 0 0 !important;
 }
 </style>
