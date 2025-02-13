dbc5538d9ef85207b773f51a654936a77f3d0ad0
Anand Kumar <ak05173@st.habib.edu.pk>
2025-02-13 11:09:43 -0700

    Replaced firebase config with env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 import { getAnalytics } from "firebase/analytics";
-import config from '../config';
 
 const firebaseConfig = {
-  apiKey: config.firebase.apiKey,
-  authDomain: config.firebase.authDomain,
-  projectId: config.firebase.projectId,
-  storageBucket: config.firebase.storageBucket,
-  messagingSenderId: config.firebase.messagingSenderId,
-  appId: config.firebase.appId,
-  measurementId: config.firebase.measurementId
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_DOMAIN,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
